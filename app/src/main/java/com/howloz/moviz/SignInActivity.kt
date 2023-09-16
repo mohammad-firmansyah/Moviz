057d0f2fbf06ab59710e76071630f0640edb9537
“Mohammad <firmansmoh842@gmail.com>
2023-09-16 11:14:41 +0700

    add new page onboarding two three . and sigin

---
+++ app/src/main/java/com/howloz/moviz/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.howloz.moviz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
---
+++ app/src/main/java/com/howloz/moviz/onboarding/OnboardingThreeActivity.kt
@@ -0,0 +1,12 @@
+package com.howloz.moviz.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.howloz.moviz.R
+
+class OnboardingThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_three)
+    }
+}
